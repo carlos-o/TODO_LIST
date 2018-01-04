d5ab776ca819f4822392baef64a4e24f4754c8e2
carlos-o <30291470+carlos-o@users.noreply.github.com>
2018-01-04 08:54:13 -0400

    fix

--- config/settings.py
+++ config/settings.py
@@ -158,7 +158,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'carlosolivero2@gmail.com'
-EMAIL_HOST_PASSWORD = '7410136205'
+EMAIL_HOST_PASSWORD = ''
 
 #jwt
 JWT_AUTH = {
